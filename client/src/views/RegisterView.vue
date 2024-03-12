ed48e884aa238f298e5e4c3729edc9ede20f4a0e
Omar Aglan <Omar.aglan91@gmail.com>
2024-03-12 04:55:26 +0200

    Register View in Vue.js and simple Registration End Point

---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AuthenticationService from '@/services/AuthenticationService';
+import { ref } from 'vue';
+
+const username = ref('');
+const password = ref('');
+
+async function register() {
+  const response = await AuthenticationService.register({
+    username: username.value,
+    password: password.value,
+  });
+  // Handle registration response here (e.g., success message, redirect)
+  console.log(response.data);
+}
+</script>
+<template>
+    <div>
+        <h1>Register</h1>
+        <br>
+        <input type="username" name="username" v-model="username" placeholder="Username">
+        <br>
+        <input type="password" name="password" v-model="password" placeholder="Password">
+        <br>
+        <button @click="register">Submit</button>
+    </div>
+</template>
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,3 @@
-console.log('hello world')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -9,9 +8,21 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.get('/', (req, res) => {
   res.send({
-    message: 'hello world'
+    message: 'hello world, This is the server index page'
   })
 })
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.username}! Your user was registered! Have fun!`
+  })
+})
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'server is running on port:' + ' 8081!'
+  })
+})
+
 app.listen(process.env.PORT || 8081)
+console.log('server is running on port:' + ' or 8081!')
